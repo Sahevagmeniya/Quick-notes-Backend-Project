7203e9393ca20c28d950104fdd7c954487fa4122
SahevagMeniya <sdmeniya@gmail.com>
2025-11-04 10:28:22 +0530

    PORT and Scripts minor changes for fix deployment

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ app.use("/", homeRouters);
 app.use("/auth", authRouters);
 app.use("/notes", notesRouters);
 
-app.listen(process.env.PORT, () =>
+app.listen(process.env.PORT, "0.0.0.0", () =>
   console.log("Server is running at PORT:", process.env.PORT)
 );
